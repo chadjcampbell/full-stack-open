0c824fd8e21e831c3e99d987a5120c34d3b13eb4
chadjcampbell <chadjcampbell@gmail.com>
2024-02-27 17:46:55 -0500

    p1 - exercise 1.5 - refactor courseinfo

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -1,24 +1,30 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const total = exercises1 + exercises2 + exercises3;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  const total = course.parts.reduce((acc, part) => {
+    acc += part.exercises;
+    return acc;
+  }, 0);
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={total} />
     </div>
   );
@@ -28,40 +34,32 @@ const Header = ({ course }: { course: string }) => {
   return <h1>{course}</h1>;
 };
 
-type ContentProps = {
-  part1: string;
-  part2: string;
-  part3: string;
-  exercises1: number;
-  exercises2: number;
-  exercises3: number;
+type PartsType = {
+  name: string;
+  exercises: number;
 };
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}: ContentProps) => {
+type ContentProps = {
+  parts: PartsType[];
+};
+
+const Content = ({ parts }: ContentProps) => {
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
     </>
   );
 };
 
 type PartProps = {
-  part: string;
-  exercises: number;
+  part: PartsType;
 };
-const Part = ({ part, exercises }: PartProps) => {
+const Part = ({ part }: PartProps) => {
   return (
     <p>
-      {part} {exercises}
+      {part.name} {part.exercises}
     </p>
   );
 };
