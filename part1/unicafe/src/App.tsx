276bc46118fbf7bc7ea1da4ca5dd351feb852fb5
ChadHomeLaptop <chadjcampbell@gmail.com>
2024-02-27 21:25:01 -0500

    p1 - unicafe done

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -6,35 +6,101 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+
   return (
     <main>
-      <h1>Give Feedback</h1>
-      <button
-        onClick={() => {
-          setGood((prev) => prev + 1);
-        }}
-      >
-        Good
-      </button>
-      <button
-        onClick={() => {
-          setNeutral((prev) => prev + 1);
-        }}
-      >
-        Neutral
-      </button>
-      <button
-        onClick={() => {
-          setBad((prev) => prev + 1);
-        }}
-      >
-        Bad
-      </button>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <section>
+        <h1>Give Feedback</h1>
+        <button
+          onClick={() => {
+            setGood((prev) => prev + 1);
+          }}
+        >
+          Good
+        </button>
+        <button
+          onClick={() => {
+            setNeutral((prev) => prev + 1);
+          }}
+        >
+          Neutral
+        </button>
+        <button
+          onClick={() => {
+            setBad((prev) => prev + 1);
+          }}
+        >
+          Bad
+        </button>
+      </section>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positive={positive}
+      />
     </main>
+  );
+};
+
+type StatisticsProps = {
+  good: number;
+  neutral: number;
+  bad: number;
+  total: number;
+  average: number;
+  positive: number;
+};
+
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  average,
+  positive,
+}: StatisticsProps) => {
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <section>
+      <h2>Statistics</h2>
+      <table>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <br />
+        <StatisticLine text="Total" value={total} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={positive} />
+      </table>
+    </section>
+  );
+};
+
+type StatisticLineProps = {
+  text: string;
+  value: number;
+};
+
+const StatisticLine = ({ text, value }: StatisticLineProps) => {
+  return (
+    <tr>
+      <td>{text}:</td>
+      <td>{value}</td>
+      {text == "Positive" && "%"}
+    </tr>
   );
 };
 
