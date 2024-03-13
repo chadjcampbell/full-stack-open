f3198bd56d3f48ac7940512d4cadbd331150dda9
ChadHomeLaptop <chadjcampbell@gmail.com>
2024-03-13 09:25:54 -0400

    p2 - countries - start filtering countries

--- part2/countries/src/App.tsx
+++ part2/countries/src/App.tsx
@@ -4,16 +4,35 @@ import axios from "axios";
 
 function App() {
   const [filter, setFilter] = useState<string>("");
-  const [countries, setCountries] = useState([]);
+  const [allCountries, setAllCountries] = useState([]);
+
+  const filteredCountries = allCountries.filter((c) =>
+    c.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
 
   useEffect(() => {
-    axios.get("https://studies.cs.helsinki.fi/restcountries/api/all");
+    const getAllCountries = async () => {
+      const res = await axios.get(
+        "https://studies.cs.helsinki.fi/restcountries/api/all"
+      );
+      setAllCountries(res.data);
+    };
+    if (allCountries.length === 0) {
+      getAllCountries();
+    }
   }, []);
+  console.log(allCountries);
+
   return (
     <main>
       <h1>Countries</h1>
       <Filter setFilter={setFilter} />
-      {/* <List/> or <CountryData/> */}
+      {allCountries.length === 0 && <p>Loading...</p>}
+      {filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : (
+        filteredCountries.map((fc) => <p>{fc.name.common}</p>)
+      )}
     </main>
   );
 }
